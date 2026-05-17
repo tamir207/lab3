d72b4158be2a9f84f8fae1b1ef54307a8117b86c
tamir207 <tamir207@gmail.com>
2026-05-17 10:30:16 +0000

    FIX: 2.A

--- task2/start.s
+++ task2/start.s
@@ -3,19 +3,19 @@ global _start
 global system_call
 global infect_file
 extern main
-
 _start:
-    pop    dword ecx
-    mov    esi,esp
-    mov     eax,ecx
-    shl     eax,2
-    add     eax,esi
-    add     eax,4
-    push    dword eax
-    push    dword esi
-    push    dword ecx
+    pop    dword ecx    ; ecx = argc
+    mov    esi,esp      ; esi = argv
+    ;; lea eax, [esi+4*ecx+4] ; eax = envp = (4*ecx)+esi+4
+    mov     eax,ecx     ; put the number of arguments into eax
+    shl     eax,2       ; compute the size of argv in bytes
+    add     eax,esi     ; add the size to the address of argv 
+    add     eax,4       ; skip NULL at the end of argv
+    push    dword eax   ; char *envp[]
+    push    dword esi   ; char* argv[]
+    push    dword ecx   ; int argc
 
-    call    main
+    call    main        ; int main( int argc, char *argv[], char *envp[] )
 
     mov     ebx,eax
     mov     eax,1
@@ -23,22 +23,22 @@ _start:
     nop
         
 system_call:
-    push    ebp
+    push    ebp             ; Save caller state
     mov     ebp, esp
-    sub     esp, 4
-    pushad
+    sub     esp, 4          ; Leave space for local var on stack
+    pushad                  ; Save some more caller state
 
-    mov     eax, [ebp+8]
-    mov     ebx, [ebp+12]
-    mov     ecx, [ebp+16]
-    mov     edx, [ebp+20]
-    int     0x80
-    mov     [ebp-4], eax
-    popad
-    mov     eax, [ebp-4]
-    add     esp, 4
-    pop     ebp
-    ret
+    mov     eax, [ebp+8]    ; Copy function args to registers: leftmost...        
+    mov     ebx, [ebp+12]   ; Next argument...
+    mov     ecx, [ebp+16]   ; Next argument...
+    mov     edx, [ebp+20]   ; Next argument...
+    int     0x80            ; Transfer control to operating system
+    mov     [ebp-4], eax    ; Save returned value...
+    popad                   ; Restore caller state (registers)
+    mov     eax, [ebp-4]    ; place returned value where caller can see it
+    add     esp, 4          ; Restore caller state
+    pop     ebp             ; Restore caller state
+    ret                     ; Back to caller
 
 infect_file:
     push    ebp
